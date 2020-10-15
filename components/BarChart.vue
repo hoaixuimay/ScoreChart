8e8b5e365f1a521179663d4c507b4480dc5a9d90
[Vision] Hoai Pham <hoai.pham@axonactive.com>
2020-10-15 18:22:07 +0700

    added

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -1,4 +1,15 @@
 <template>
+  <div class="chart">
+    <div class="chart__header"></div>
+    <div class="chart__x"></div>
+    <div class="chart__content">
+      <div class="chart__item" v-for="(item,index) of data" :key="index">
+                        {{item}}
+                    </div>
+    </div>
+    <div class="chart__y"></div>
+    <div class="chart__footer"></div>
+  </div>
 </template>
 
 <script>
@@ -10,3 +21,11 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.chart__item {
+  height: 100px;
+  width: 100px;
+  position: relative;
+}
+</style>
